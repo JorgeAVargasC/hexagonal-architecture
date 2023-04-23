3d718ce31efa53e7d2b9faae5556d844cdc7e332
JorgeAVargasC <javargas1209@gmail.com>
2023-04-23 16:41:54 -0500

    [ADD] hexagonal architecture

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+const path = require('path')
 
-// https://vitejs.dev/config/
+//* https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/components': path.resolve(__dirname, './src/components/'),
+      '@/atoms': path.resolve(__dirname, './src/atoms/'),
+      '@/config': path.resolve(__dirname, './src/config/'),
+      '@/layouts': path.resolve(__dirname, './src/layouts/'),
+      '@/hooks': path.resolve(__dirname, './src/hooks/'),
+      '@/routes': path.resolve(__dirname, './src/routes/'),
+      '@/services': path.resolve(__dirname, './src/services/'),
+      '@/pages': path.resolve(__dirname, './src/pages/'),
+      '@/utils': path.resolve(__dirname, './src/utils/'),
+      '@/images': path.resolve(__dirname, './src/assets/images/'),
+      '@/docs': path.resolve(__dirname, './src/assets/docs/'),
+      '@/icons': path.resolve(__dirname, './src/assets/icons/'),
+      '@/videos': path.resolve(__dirname, './src/assets/videos/'),
+      https: 'agent-base'
+    }
+  },
+  plugins: [react(), svgr()],
+  server: {
+    host: true
+  }
 })
