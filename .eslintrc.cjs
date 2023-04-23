207c2ba0f7f2ff7cf75fe6f14ca2fc576a72995e
JorgeAVargasC <javargas1209@gmail.com>
2023-04-23 13:05:20 -0500

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,126 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+    {
+      files: [
+        '*.js',
+        '*.jsx',
+        '*.ts',
+        '*.tsx'
+      ],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Packages `react` related packages come first.
+              ['^react', '^@?\\w'],
+              // Internal packages.
+              [
+                '^(.*config)(/.*|$)'
+              ],
+              [
+                '^(.*images)(/.*|$)'
+              ],
+              [
+                '^(.*icons)(/.*|$)'
+              ],
+              [
+                '^(.*docs)(/.*|$)'
+              ],
+              [
+                '^(.*utils)(/.*|$)'
+              ],
+              [
+                '^(.*routes)(/.*|$)'
+              ],
+              [
+                '^(.*pages)(/.*|$)'
+              ],
+              [
+                '^(.*layouts)(/.*|$)'
+              ],
+              [
+                '^(.*components)(/.*|$)'
+              ],
+              [
+                '^(.*recoil)(/.*|$)'
+              ],
+              [
+                '^(.*atoms)(/.*|$)'
+              ],
+              [
+                '^(.*hooks)(/.*|$)'
+              ],
+              [
+                '^(.*services)(/.*|$)'
+              ],
+              // Side effect imports.
+              [
+                '^\\u0000'
+              ],
+              // Parent imports. Put `..` last.
+              [
+                '^\\.\\.(?!/?$)',
+                '^\\.\\./?$'
+              ],
+              // Other relative imports. Put same-folder imports and `.` last.
+              [
+                '^\\./(?=.*/)(?!/?$)',
+                '^\\.(?!/?$)',
+                '^\\./?$'
+              ],
+              // Style imports.
+              [
+                '^.+\\.?(css)$'
+              ]
+            ]
+          }
+        ]
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    ecmaFeatures: { jsx: true },
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'simple-import-sort'
+  ],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'no-console': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-unused-vars': 'warn',
+    indent: [
+      'error',
+      2
+    ],
+    'react/self-closing-comp': 'error',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-key': 'off',
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    'jsx-quotes': [
+      'error',
+      'prefer-single'
+    ]
+  }
+}
